1d587a2cfbca508f13a2b5568be53a086539174a
mwmcclure7 <106849204+mwmcclure7@users.noreply.github.com>
2024-03-30 14:29:17 -0400

    Temporarily stops moving mouse when failsafe triggered

--- mouse_mover.py
+++ mouse_mover.py
@@ -11,14 +11,17 @@ except:
 screen_width, screen_height = pyautogui.size()
 
 while True:
-    # Move the mouse cursor randomly
-    x_offset = random.randint(0, screen_width)
-    y_offset = random.randint(0, screen_height)
-    base_duration = abs(x_offset/screen_width)+abs(y_offset/screen_height)
-    duration = random.uniform(base_duration*0.5, base_duration*2)
+    try:
+        # Move the mouse cursor randomly
+        x_offset = random.randint(0, screen_width)
+        y_offset = random.randint(0, screen_height)
+        base_duration = abs(x_offset/screen_width)+abs(y_offset/screen_height)
+        duration = random.uniform(base_duration*0.5, base_duration*2)
 
-    # Uncomment this to disable the fail-safe
-    # pyautogui.FAILSAFE = False
+        # Uncomment this to disable the fail-safe
+        pyautogui.FAILSAFE = True
 
-    pyautogui.moveTo(x_offset, y_offset, duration=duration, tween=pyautogui.easeInOutQuad)
-    # time.sleep(random.random())
+        pyautogui.moveTo(x_offset, y_offset, duration=duration, tween=pyautogui.easeInOutQuad)
+    except:
+        # Stops moving mouse temporarily when mouse moves to the corner of the screen
+        time.sleep(20)
